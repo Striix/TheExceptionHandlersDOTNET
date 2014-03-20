c24278bf7541b09799e79ba68f450d5349e74604
Striix <astriid_dl@hotmail.com>
2014-03-20 09:41:19 +0100

    Lay-out

--- Hogent Stages.Repository/Stages/Model/Bedrijf.cs
+++ Hogent Stages.Repository/Stages/Model/Bedrijf.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using Hogent_Stages.Repository.Stages.CustomAnnotations;
+using System.Globalization;
 using System.Web;
 
 using System.ComponentModel;
@@ -15,43 +17,51 @@ namespace Hogent_Stages.Repository.Stages
         [Required(ErrorMessage = "{0} is verplicht")]
         public String bedrijfsNaam { get; set; }
 
-        [Display(Name = "De straatnaam")]
+        [Display(Name = "Straat")]
+        [Required(ErrorMessage = "{0} is verplicht")]
         public String straat { get; set; }
 
-        [Display(Name = "Het nummer")]
+        [Display(Name = "Huisnummer")]
+        [Required(ErrorMessage = "{0} is verplicht")]
+        [RegularExpression("\\d+", ErrorMessage = "{0} moet een getal zijn")]
         public int nummer { get; set; }
 
-        [Display(Name = "De postcode")]
-        public int postcode { get; set; }
+        [Display(Name = "Postcode")]
+        [Required(ErrorMessage = "{0} is verplicht")]
+        [RegularExpression("\\d+", ErrorMessage = "{0} moet een getal zijn")]
+        public String postcode { get; set; }
 
-        [Display(Name = "De plaatsnaam")]
+        [Display(Name = "Plaatsnaam")]
         [Required(ErrorMessage = "{0} is verplicht")]
         public String plaats { get; set; }
 
+        [Display(Name = "URL")]
+        [Required(ErrorMessage = "{0} is verplicht")]
         public string url { get; set; }
 
-        [Display(Name = "Het e-mailadres")]
+        [Display(Name = "E-mailadress")]
         [Required(ErrorMessage = "{0} is verplicht")]
-        [EmailAddress(ErrorMessage = "{0} moet een geldig emailadres zijn.")]
+        [EmailAddress(ErrorMessage = "{0} moet een geldig zijn.")]
         public string email { get; set; }
 
-        [Display(Name = "Telefoonnummer van het bedrijf")]
+        [Display(Name = "Telefoonnummer")]
         [Required(ErrorMessage = "{0} is verplicht")]
         public string telefoon { get; set; }
 
         public string bereikbaarheid { get; set; }
 
-        [Display(Name = "Een zelfgekozen wachtwoord")]
+        [Display(Name = "Wachtwoord")]
         [Required(ErrorMessage = "{0} is verplicht")]
         public string wachtwoord { get; set; }
 
         [Display(Name = "Bevestig wachtwoord")]
         [Required(ErrorMessage = "{0} is verplicht")]
-        [Compare("wachtwoord")]
+        [Compare("wachtwoord", ErrorMessage = "Wachtwoorden komen niet overeen.")]
         public string bevestigWachtwoord { get; set; }
 
-        [Display(Name = "Naam van de contactpersoon")]
+        [Display(Name = "Contactpersoon")]
         [Required(ErrorMessage = "{0} is verplicht")]
         public string contactPersoon { get; set; }
+
     }
 }
